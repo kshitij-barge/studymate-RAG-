b3fac498d91654ae26243534172a5b30752fe457
Kshitij <kshitijbarge15@gmail.com>
2026-05-02 11:52:04 +0530

    Fix LangChain text splitter import

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import tempfile
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_mistralai import ChatMistralAI
 from langchain_community.vectorstores import FAISS
